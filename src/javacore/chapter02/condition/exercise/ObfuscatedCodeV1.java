9687cbfb78b81fe5aca385af982e2a8e76031b72
Maxime Belin <maxime.dev9424@gmail.com>
2025-03-10 22:02:33 +0100

    Chapitre 2 : Obfuscated Code V1

--- src/javacore/chapter02/condition/exercise/ObfuscatedCodeV1.java
+++ src/javacore/chapter02/condition/exercise/ObfuscatedCodeV1.java
@@ -1,4 +1,32 @@
 package javacore.chapter02.condition.exercise;
 
 public class ObfuscatedCodeV1 {
+
+    public static void main(String[] args) {
+
+        int typeOfVehicle = 3; // Type de véhicule (1 = Electrique, 2 = Essence, 3 = Diesel)
+
+        double price = 100;
+
+        boolean isClean = false;
+        double ecologicalBonus = 0.03;
+
+        double environmentalTax = 0;
+
+        if (typeOfVehicle == 1) {
+            environmentalTax = price * 0.10; // Montant de taxe pour véhicule électrique
+        }
+        if (typeOfVehicle == 2) {
+            environmentalTax = price * 0.20; // Montant de taxe pour véhicule essence
+        }
+        if (typeOfVehicle == 3) {
+            environmentalTax = price * 0.35; // Montant de taxe pour véhicule diesel
+        }
+        if (isClean) {
+            environmentalTax -= (price * ecologicalBonus); // Application d'un bonus écologique si le véhicule est propre
+        }
+
+        System.out.println("Montant de la taxe environnemental : " + environmentalTax + "€");
+
+    }
 }
