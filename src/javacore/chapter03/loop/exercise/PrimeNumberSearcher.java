5d2b7d109d4c435ee24fd17befd1078a3387af5c
Maxime Belin <maxime.dev9424@gmail.com>
2025-04-01 02:57:55 +0200

    Chapitre 3 : Prime Number Searcher

--- src/javacore/chapter03/loop/exercise/PrimeNumberSearcher.java
+++ src/javacore/chapter03/loop/exercise/PrimeNumberSearcher.java
@@ -6,27 +6,27 @@ public class PrimeNumberSearcher {
 
     public static void main(String[] args){
 
-        int count = 1;
-        int number = 1;
+        int primeNumbersFound = 1;
+        int potentialPrimeNumber = 1;
 
         System.out.println("Liste des 50 premiers nombres premiers : ");
 
-        while(count <= 50){
+        while(primeNumbersFound <= 50){
 
             boolean isPrime = true;
 
-            for (int divisor = 2; divisor <= number - 1; divisor++) {
-                if (number % divisor == 0) {
+            for (int potentialDivisor = 2; potentialDivisor < potentialPrimeNumber; potentialDivisor++) {
+                if (potentialPrimeNumber % potentialDivisor == 0) {
                     isPrime = false;
                 }
             }
 
             if (isPrime) {
-                System.out.print(number + " ");
-                count++;
+                System.out.print(potentialPrimeNumber + " ");
+                primeNumbersFound++;
             }
 
-            number++;
+            potentialPrimeNumber++;
         }
 
     }
