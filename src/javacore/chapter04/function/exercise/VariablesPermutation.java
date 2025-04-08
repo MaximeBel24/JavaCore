458777e3d7a84df35cc7efe6aba357f8093ae9df
Maxime Belin <maxime.dev9424@gmail.com>
2025-04-08 17:58:58 +0200

    Chapitre 4 : Variables Permutation

--- src/javacore/chapter04/function/exercise/VariablesPermutation.java
+++ src/javacore/chapter04/function/exercise/VariablesPermutation.java
@@ -13,6 +13,6 @@ public class VariablesPermutation {
     }
 
     public static int sum(int number1, int number2, int number3){
-        return sum(number1, number2) + number3;
+        return sum(sum(number1, number2), number3);
     }
 }
