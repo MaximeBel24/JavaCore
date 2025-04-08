7492fc91cfbd94942a2b23c50fcd593f45322b0f
Maxime Belin <maxime.dev9424@gmail.com>
2025-04-08 17:11:06 +0200

    Chapitre 4 : Variable Permutation

---
+++ src/javacore/chapter04/function/exercise/VariablesPermutation.java
@@ -0,0 +1,17 @@
+package javacore.chapter04.function.exercise;
+
+public class VariablesPermutation {
+    public static void main(String[] args){
+        System.out.println("Résultat de sum avec 2 arguments : " + sum(20, 5));
+
+        System.out.println("Résultat de sum avec 3 arguments : " + sum(20, 5, 12));
+    }
+
+    public static int sum(int number1, int number2){
+        return number1 + number2;
+    }
+
+    public static int sum(int number1, int number2, int number3){
+        return sum(number1, number2) + number3;
+    }
+}
