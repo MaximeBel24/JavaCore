30fb60edb06cd9b4c8f702b8f880f92826e90a28
Maxime Belin <maxime.dev9424@gmail.com>
2025-05-12 15:27:23 +0200

    Chapitre 5 : Pen

---
+++ src/javacore/chapter05/object/exercise/Pen.java
@@ -0,0 +1,32 @@
+package javacore.chapter05.object.exercise;
+
+public class Pen {
+
+    String name;
+    String color;
+    String producer;
+
+    int quantity;
+
+    double price;
+    double weight;
+
+    public void displayInformation() {
+        System.out.println("Nom: " + name +", Couleur: " + color + ", Prix: " + price + "€, Quantité: " + quantity + ", Fabricant: " + producer + ", Poids: " + weight + "g");
+    }
+
+    public static void main(String[] args) {
+
+        Pen blueBic = new Pen();
+
+        blueBic.name = "Stylo";
+        blueBic.color = "Bleu";
+        blueBic.producer = "Bic";
+        blueBic.quantity = 10;
+        blueBic.price = 2.50;
+        blueBic.weight = 15;
+
+        blueBic.displayInformation();
+    }
+
+}
