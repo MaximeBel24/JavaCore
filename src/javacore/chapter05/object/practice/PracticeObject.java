0c0e493ddbfe7966c43ff15f8b9e91f41b245e0e
Maxime Belin <maxime.dev9424@gmail.com>
2025-05-13 23:43:48 +0200

    Chapitre 5 : Practice Object

--- src/javacore/chapter05/object/practice/PracticeObject.java
+++ src/javacore/chapter05/object/practice/PracticeObject.java
@@ -4,25 +4,11 @@ public class PracticeObject {
 
     public static void main(String[] args) {
 
-        PracticeObjectCar car1 = new PracticeObjectCar();
-        car1.brand = "Ferrari";
-        car1.model = "F458";
-        car1.color = "Rouge";
-        car1.price = 260000;
-        car1.odometer = 0;
-        car1.serialNumber = "FV5463SDFGUDG543F546HG";
-
+        PracticeObjectCar car1 = new PracticeObjectCar("FV5463SDFGUDG543F546HG","Ferrari","F458","Rouge", 260000);
         car1.run(2000);
         car1.displayInformation();
 
-        PracticeObjectCar car2 = new PracticeObjectCar();
-        car2.brand = "Ford";
-        car2.model = "Mustang";
-        car2.color = "Jaune";
-        car2.price = 120000;
-        car2.odometer = 0;
-        car2.serialNumber = "VRGHVJ46571RGDB454BRT";
-
+        PracticeObjectCar car2 = new PracticeObjectCar("VRGHVJ46571RGDB454BRT", "Ford", "Mustang", "Jaune", 120000);
         car2.run(4000);
         car2.displayInformation();
 
--- src/javacore/chapter05/object/practice/PracticeObjectCar.java
+++ src/javacore/chapter05/object/practice/PracticeObjectCar.java
@@ -10,6 +10,17 @@ public class PracticeObjectCar {
     double price;
     double odometer;
 
+    public PracticeObjectCar(String serialNumber, String brand, String model, String color, double price) {
+        this.serialNumber = serialNumber;
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.price = price;
+        this.odometer = 0;
+
+        this.displayInformation();
+    }
+
     public double run(double distance) {
         System.out.println("La voiture roule pendant " + distance + " kms.");
         odometer += distance;
@@ -17,6 +28,11 @@ public class PracticeObjectCar {
     }
 
     public void displayInformation() {
-        System.out.println("N° série : " + serialNumber + " | Marque : " + brand + " | Modèle : " + model + " | Couleur : " + color + " | Kilomètrage : " + odometer);
+        System.out.println("N° série : " + this.serialNumber +
+                " | Marque : " + this.brand +
+                " | Modèle : " + this.model +
+                " | Couleur : " + this.color +
+                " | Kilomètrage : " + this.odometer
+        );
     }
 }
