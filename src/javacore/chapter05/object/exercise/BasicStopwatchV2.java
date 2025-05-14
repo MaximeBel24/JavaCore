c9ad7e4e5d46d9239d975044f21a92c6fdcbe155
Maxime Belin <maxime.dev9424@gmail.com>
2025-05-14 11:14:28 +0200

    Chapitre 5 : Basic Stopwatch V3

--- src/javacore/chapter05/object/exercise/BasicStopwatchV2.java
+++ src/javacore/chapter05/object/exercise/BasicStopwatchV2.java
@@ -2,9 +2,18 @@ package javacore.chapter05.object.exercise;
 
 public class BasicStopwatchV2 {
 
+    String name;
+
     int seconds;
     int minutes;
     int hours;
+
+    public BasicStopwatchV2(String name, int seconds, int minutes, int hours) {
+        this.name = name;
+        this.seconds = seconds;
+        this.minutes = minutes;
+        this.hours = hours;
+    }
 
     public void launch() throws InterruptedException {
         while(true) {
@@ -28,15 +37,11 @@ public class BasicStopwatchV2 {
     }
 
     public void displayTime() {
-        System.out.printf("%02d:%02d:%02d%n", hours, minutes, seconds);
+        System.out.printf("%s : %02d:%02d:%02d%n", name, hours, minutes, seconds);
     }
 
     public static void main(String[] args) throws InterruptedException {
-        BasicStopwatchV2 stopwatch = new BasicStopwatchV2();
-
-        stopwatch.seconds = 0;
-        stopwatch.minutes = 0;
-        stopwatch.hours = 0;
+        BasicStopwatchV2 stopwatch = new BasicStopwatchV2("Oeufs Durs", 0, 0, 0);
 
         stopwatch.launch();
     }
